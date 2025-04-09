c43b72c6a00bb1e8923f0f425bd96cd0bc03c158
Paskmark <p.cebriagomez@edu.gva.es>
2025-04-09 18:55:09 +0200

    Add files via upload

---
+++ TELEDANA/INCLUDES/footer.php
@@ -0,0 +1,9 @@
+// 3. /includes/footer.php
+<?php
+?>
+<footer>
+    <p>&copy; 2025 DANA - Todos los derechos reservados</p>
+    <p><a href="/DANA/terminos.php">Términos de Uso</a> | <a href="/DANA/proteccion_datos.php">Protección de Datos</a></p>
+</footer>
+</body>
+</html>
