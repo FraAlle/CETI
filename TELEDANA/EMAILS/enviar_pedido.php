48ba5bd82587b9415e171e57d9554fcb0c6205b8
Borja <131769883+3orja@users.noreply.github.com>
2025-04-11 18:31:47 +0200

    Delete TELEDANA directory

--- TELEDANA/EMAILS/enviar_pedido.php
+++
@@ -1,7 +0,0 @@
-// 6. /emails/enviar_pedido.php
-<?php
-function enviarCorreoPedido($email, $mensaje) {
-    $headers = "From: no-reply@dana.com";
-    mail($email, "Confirmación de Pedido", $mensaje, $headers);
-}
-?>
--- TELEDANA/INCLUDES/footer.php
+++
@@ -1,9 +0,0 @@
-// 3. /includes/footer.php
-<?php
-?>
-<footer>
-    <p>&copy; 2025 DANA - Todos los derechos reservados</p>
-    <p><a href="/DANA/terminos.php">Términos de Uso</a> | <a href="/DANA/proteccion_datos.php">Protección de Datos</a></p>
-</footer>
-</body>
-</html>
